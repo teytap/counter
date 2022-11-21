d87cae4e0fb6a9065a05fae200a0fde85143a842
Mehtap Tataroğlu <teytap@gmail.com>
2022-11-21 18:40:40 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let count = 0;
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+}
+
+function save() {
+  let countString = count + " - ";
+  saveEl.textContent += countString;
+  count = 0;
+  countEl.innerText = count;
+  console.log(count);
+}
